fe2135020ddc798706ef67706992b17b07df62fc
Dewey Dunnington <dewey@fishandwhistle.net>
2017-03-31 03:10:29 -0300

    add native raster functions and update docs

--- R/geom-spraster.R
+++ R/geom-spraster.R
@@ -20,7 +20,34 @@
 #'
 #' @importFrom ggplot2 fortify
 #'
-fortify.Raster <- function(model, data = NULL, ...) {
+#' @examples
+#' # use with ggplot()
+#' df <- fortify(longlake_osm)
+#' ggplot(df, aes(x, y, fill = band1)) + geom_raster() +
+#'   coord_fixed()
+#' # identical usage with ggraster()
+#' ggraster(longlake_osm, aes(fill = band1))
+#'
+#' # use long format to facet by band
+#' dflong <- fortify(longlake_osm, format = "long")
+#' ggplot(dflong, aes(x, y, fill = value)) +
+#'   geom_raster() + facet_wrap(~band) +
+#'   coord_fixed()
+#'
+#' \donttest{
+#' # can use on other raster types as well
+#' x <- rosm::osm.raster("gatineau qc")
+#' ggraster(x, aes(fill = band1))
+#' df <- fortify(x, format = "long")
+#' ggplot(df, aes(x, y, fill = value)) +
+#'   geom_raster() + facet_wrap(~band) +
+#'   coord_fixed()
+#' }
+#'
+fortify.Raster <- function(model, data = NULL, format = c("wide", "long"), ...) {
+  # match format arg
+  format <- match.arg(format)
+
   # I used 'x', ggplot2 used 'model'...
   x <- model
 
@@ -36,8 +63,104 @@ fortify.Raster <- function(model, data = NULL, ...) {
   fused$x <- bbox[1,1]+(fused$x-1)/x@ncols*(bbox[1,2]-bbox[1,1])
   fused$y <- bbox[2,1]+(fused$y-x@nrows)/x@nrows*(bbox[2,1]-bbox[2,2])
 
-  # return the data frame
-  fused
+  if(format == "wide") {
+    # return the data frame
+    fused
+  } else {
+    reshape2::melt(fused, id.vars = c("x", "y"), variable.name = "band")
+  }
+}
+
+
+#' Annotation raster using Raster* layers
+#'
+#' This is a thin wrapper around \link[ggplot2]{annotation_raster}, with the
+#' bounds arguments filled in using the bounds of the raster. Like annotation_raster,
+#' this will not adjust the extents of the plot. The coordinates used are in
+#' the coordinate system of the raster, which are likely not lat/lon.
+#'
+#' @param raster A Raster* object
+#' @param interpolate TRUE to interpolate rendering
+#' @param na.value A value to represent NAs, since a transparency band
+#'   may or may not exist for this raster. If na.value = NA, a transparency
+#'   band will be created to remove missing values from display.
+#'
+#' @return An annotation layer or list of layers (geom_spraster_rgb)
+#' @export
+#'
+#' @examples
+#' ggplot() +
+#'   annotation_spraster(longlake_osm) +
+#'   geom_spatial(longlake_waterdf, toepsg = 26920) +
+#'   coord_fixed()
+#'
+#'
+annotation_spraster <- function(raster, interpolate = FALSE, na.value = NA) {
+  if(!methods::is(raster, "Raster")) stop("Cannot use annotation_spraster with non Raster* object")
+  bbox <- raster::as.matrix(raster@extent)
+  raster <- raster::as.array(raster)
+
+  # check dims
+  dims <- dim(raster)
+  if(length(dims) != 3) stop("Raster has incorrect dimensions: ", paste(dims, collapse = ", "))
+  if(!(dims[3] %in% c(3, 4))) stop("Need a 3 or 4-band array to use annotation_spraster")
+
+  # make values between 0 and 1, if they are not already
+  vrange <- range(raster, finite = TRUE)
+  if(!all(vrange >= 0 & vrange <= 1)) {
+    if(all(vrange >= 0 & vrange <= 256)) {
+      raster <- scales::rescale(raster, from = c(0, 256))
+    } else{
+      raster <- scales::rescale(raster)
+    }
+  }
+
+  # eliminate NAs
+  if(is.na(na.value) && any(is.na(raster))) {
+    # find NA cells
+    na_cells <- is.na(raster[, , 1]) | is.na(raster[, , 2]) |
+      is.na(raster[, , 3])
+
+    # grid doesn't do non-finite values, so we need to set the transparency band
+    # for missing cells
+    if(dim(raster)[3] == 4) {
+      tband <- raster[ , , 4, drop = FALSE]
+    } else {
+      tband <- array(1, dim(raster)[1:2])
+    }
+
+    # set alpha to NA cells to 0
+    tband[na_cells] <- 0
+
+    # bind it to the original raster
+    raster <- abind::abind(raster[, , 1:3, drop = FALSE], tband)
+
+    # set NA values to 0
+    raster[is.na(raster)] <- 0
+  } else {
+    raster[is.na(raster)] <- na.value
+  }
+
+  # call ggplot2's annotation_raster
+  ggplot2::annotation_raster(raster, bbox[1, 1], bbox[1, 2], bbox[2, 1], bbox[2, 2],
+                             interpolate = interpolate)
+}
+
+#' @rdname annotation_spraster
+#' @export
+geom_spraster_rgb <- function(raster, interpolate = FALSE, na.value = NA) {
+  if(!methods::is(raster, "Raster")) stop("Cannot use geom_spraster_rgb with non Raster* object")
+
+  # wraps around annotation_spraster using a blank geometry to set bounds
+  bbox <- raster::as.matrix(raster@extent)
+  data <- data.frame(x = bbox[1, ], y = bbox[2, ])
+
+  # returns a list with both layers
+  list(
+    geom_point(ggplot2::aes_string("x", "y"), data = data, alpha = 0, inherit.aes = FALSE,
+               show.legend = FALSE),
+    annotation_spraster(raster, interpolate = interpolate, na.value = na.value)
+  )
 }
 
 
@@ -46,7 +169,9 @@ fortify.Raster <- function(model, data = NULL, ...) {
 #' Similar to the \link{geom_spatial} family for Spatial* objects, this method
 #' plots a spatial raster as a spatial raster. Note that projecting (or un-projecting)
 #' this layer will likely result in odd results (namely that geom_raster will not
-#' function properly or at all).
+#' function properly or at all). As a convenience, \code{ggraster()} is provided,
+#' which replaces the call to \code{coord_map()} from \link{ggspatial} with a call
+#' to \code{coord_fixed()}.
 #'
 #' @param data A Raster* object
 #' @param mapping A mapping
@@ -67,7 +192,11 @@ fortify.Raster <- function(model, data = NULL, ...) {
 #' @export
 #'
 #' @examples
-#' ggplot() + geom_spatial(longlake_osm)
+#' # standard ggplot syntax
+#' ggplot() + geom_spatial(longlake_osm, aes(fill = band1)) + coord_fixed()
+#'
+#' # or use ggraster()
+#' ggraster(longlake_osm, aes(fill = band1))
 #'
 geom_spatial.Raster <- function(data, mapping = NULL, show.legend = TRUE, inherit.aes=FALSE,
                                 position = "identity", fromepsg=NULL, toepsg=NULL,
@@ -90,17 +219,107 @@ geom_spatial.Raster <- function(data, mapping = NULL, show.legend = TRUE, inheri
   }
 
   # set the defaults
-  final_mapping <- ggplot2::aes_string("x", "y", fill = "band1")
+  final_mapping <- ggplot2::aes_string("x", "y")
   final_mapping <- c(mapping, final_mapping)
   class(final_mapping) <- "uneval"
 
-  # return layer
-  layer(
+  # create layer
+  gglayer <- layer(
     stat = stat, data = fortify.Raster(data), mapping = final_mapping, geom = geom,
     show.legend = show.legend, inherit.aes = inherit.aes, position = "identity",
     params=c(projections, list(...)))
+
+  # if stat is rgba, we need to return a list, since this stat
+  # makes no sense without scale_alpha_identity and scale_fill_identity
+  if(stat == "rgba") {
+    list(gglayer, scale_alpha_identity(), scale_fill_identity())
+  } else {
+    # return layer
+    gglayer
+  }
 }
 
+#' @rdname geom_spatial.Raster
+#' @export
+ggraster <- function(data, mapping = NULL, ...) {
+  if(!methods::is(data, "Raster")) stop("ggraster is not applicable for class of type ", class(data))
+  ggplot() + geom_spatial(data, mapping = mapping, ...)
+}
+
+#' Statistic to create RGB fill values
+#'
+#' Usually used on conjunction with \link[ggplot2]{geom_raster} or
+#' \link{geom_spatial.Raster}. This function appears to work, however
+#' it is slow, and in general, only good for very small datasets.
+#'
+#' @param mapping A mapping created with \link[ggplot2]{aes}
+#' @param data A data.frame
+#' @param geom The geometry to use
+#' @param position The position adjustment to use
+#' @param ... Passed to \link[ggplot2]{geom_raster}
+#' @param limits_red Data limits from which to scale red values. Use NULL to
+#'   perform no transformation, or NA to use the data value without transformation.
+#' @param limits_green Data limits from which to scale green values
+#' @param limits_blue Data limits from which to scale blue values
+#' @param limits_alpha Data limits from which to scale alpha values
+#'
+#' @return A ggplot2 layer object
+#' @export
+#'
+#' @examples
+#' # using stat_rgba()
+#' ggplot(longlake_osm, aes(x, y)) +
+#'   stat_rgba(aes(red = band1, green = band2, blue = band3, alpha = 1),
+#'             limits_red = NULL, limits_green = NULL, limits_blue = NULL,
+#'             limits_alpha = NULL, interpolate = TRUE) +
+#'   coord_fixed()
+#'
+#' # using ggraster() with stat = 'rgba'
+#' ggraster(longlake_osm, aes(red = band1, green = band2, blue = band3, alpha = 1),
+#'          stat = "rgba")
+#'
+stat_rgba <- function(mapping = NULL, data = NULL, ..., limits_red = NA, limits_green = NA,
+                      limits_blue = NA, limits_alpha = NA) {
+  # return a list, since this stat is meaningless without identity scales
+  # for fill and alpha
+
+  list(
+    geom_raster(mapping = mapping, data = data, stat = StatRgba,
+                limits_red = limits_red,
+                limits_green = limits_green, limits_blue = limits_blue,
+                limits_alpha = limits_alpha, ...),
+    scale_alpha_identity(),
+    scale_fill_identity()
+  )
+}
+
+
+StatRgba <- ggplot2::ggproto("StatRgba", ggplot2::Stat,
+
+  compute_group = function(data, scales, limits_red = NA, limits_green = NA,
+                           limits_blue = NA, limits_alpha = NA) {
+
+    # rescale data between 0 and 255 for RGB, 0 and 1 for alpha
+    data$red <- rescale_item(data$red, limits_red, to = c(0, 255))
+    data$green <- rescale_item(data$green, limits_green, to = c(0, 255))
+    data$blue <- rescale_item(data$blue, limits_blue, to = c(0, 255))
+    data$alpha <- rescale_item(data$alpha, limits_alpha, to = c(0, 1))
+
+    not_finite <- !complete.cases(data[c("red", "green", "blue", "alpha")])
+
+    data$rgb <- paste0("#", do.call(paste0, lapply(data[c("red", "green", "blue")],
+                                                    as.character.hexmode, 2)))
+    data$rgb[not_finite] <- NA
+
+    data
+  },
+
+  required_aes = c("red", "green", "blue", "alpha"),
+
+  default_aes = aes(fill = ..rgb..)
+)
+
+# this rescale item appears in StatRgba
 rescale_item <- function(values, limits, to, identity = 0) {
   # need a zero range value (probably 0)
   if(scales::zero_range(range(values, finite = TRUE))) {
@@ -124,46 +343,3 @@ rescale_item <- function(values, limits, to, identity = 0) {
     stop("Don't know how to rescale with limits ", values)
   }
 }
-
-
-#' Statistic to map rgb values
-#'
-#' @param mapping A mapping created with \link[ggplot2]{aes}
-#' @param data A data.frame
-#' @param geom The geometry to use
-#' @param position The position adjustment to use
-#' @param ... Aesthetics or parameters to set
-#'
-#' @return A ggplot2 layer object
-#' @export
-#'
-stat_rgba <- function(mapping = NULL, data = NULL, geom = "point", position = "identity", ...) {
-  # TODO working on this
-  stop("not implemented")
-}
-
-StatRgba <- ggplot2::ggproto("StatRgba", ggplot2::Stat,
-
-  compute_group = function(data, scales, limits_red = NA, limits_green = NA,
-                           limits_blue = NA, limits_alpha = NA) {
-
-    # rescale data between 0 and 255 for RGB, 0 and 1 for alpha
-    data$red <- rescale_item(data$red, limits_red, to = c(0, 255))
-    data$green <- rescale_item(data$green, limits_green, to = c(0, 255))
-    data$blue <- rescale_item(data$blue, limits_blue, to = c(0, 255))
-    data$alpha <- rescale_item(data$alpha, limits_alpha, to = c(0, 1))
-
-    not_finite <- !complete.cases(data[c("red", "green", "blue", "alpha")])
-
-    data$fill <- paste0("#", do.call(paste0, lapply(data[c("red", "green", "blue")],
-                                                    as.character.hexmode, 2)))
-    data$fill[not_finite] <- NA
-    return(data)
-  },
-
-  required_aes = c("red", "green", "blue", "alpha"),
-
-  default_aes = aes(red = ..band1.., green = ..band2..,
-                    blue = ..band3.., ..band4..)
-)
-
