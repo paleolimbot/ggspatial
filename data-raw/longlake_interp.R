e045bd5bdc428fdbd81d0ad3edcc4d5fb899ddca
Dewey Dunnington <dewey@fishandwhistle.net>
2018-06-14 22:33:07 -0700

    move location of longlake data

---
+++ data-raw/longlake_interp.R
@@ -0,0 +1,95 @@
+
+library(tidyverse)
+library(sf)
+
+# create a depth raster of longlake using depth_df and outline
+ggspatial::load_longlake_data()
+
+# helper functions from the lake mophometry paper
+
+# this gets used to create a regular grid for GAM interpolation
+create_grid <- function(xy, ...) {
+  expand.grid(
+    X = seq(floor(min(xy$X)), ceiling(max(xy$X)), ...),
+    Y = seq(floor(min(xy$Y)), ceiling(max(xy$Y)), ...)
+  ) %>% as_tibble()
+}
+
+interpolate_raster <- function(sp, values, boundary = NULL, boundary_values = 0,
+                               method = gam_repl, ...) {
+
+  # create nodes object for processing
+  points <- sp %>%
+    st_zm() %>%
+    transmute(z = !!enquo(values), point_type = "input") %>%
+    st_cast("POINT", warn = FALSE)
+
+  if(!is.null(boundary)) {
+    boundary <- boundary %>%
+      st_transform(st_crs(points)) %>%
+      st_zm()
+    points <- points %>%
+      rbind(
+        boundary %>%
+          transmute(z = !!enquo(boundary_values), point_type = "boundary") %>%
+          st_cast("POINT", warn = FALSE)
+      )
+  }
+
+  rast <- method(points, ...)
+
+  if(!is.null(boundary)) {
+    rast <- raster::mask(rast, as(boundary, "Spatial"))
+  }
+
+  rast
+}
+
+gam_repl <- function(points, k = 60, quiet = TRUE, by = NULL, length.out = 100) {
+  # significant inspiration from here:
+  # https://www.fromthebottomoftheheap.net/2016/03/27/soap-film-smoothers/
+
+  if(is.null(by)) {
+    grid_gen <- function(xy) {
+      create_grid(xy, length.out = length.out)
+    }
+  } else {
+    grid_gen <- function(xy) {
+      create_grid(xy, by = by)
+    }
+  }
+
+  if(!quiet) message("Creating grid...")
+
+  # get z points, z as a data frame
+  z_values <- bind_cols(
+    points %>% unclass() %>% as_tibble() %>% select(-geometry),
+    points %>% st_coordinates() %>% as_tibble()
+  )
+
+  if(!quiet) message("Fitting GAM...")
+  s <- mgcv::s
+  tprs <- mgcv::gam(z ~ s(X, Y, k = k), data = z_values, method = "REML")
+
+  if(!quiet) message("Interpolating...")
+
+  # create grid, predict values at regularly-spaced grid locations
+  grid <- grid_gen(z_values)
+  grid$z <- predict(tprs, grid, type = "response")
+
+  # return raster with correct points
+  raster::rasterFromXYZ(grid, crs = st_crs(points)$proj4string)
+}
+
+longlake_depth_raster <- interpolate_raster(
+  longlake_depthdf,
+  values = DEPTH.M,
+  boundary = longlake_waterdf %>% slice(2)
+)
+
+raster::plot(longlake_depth_raster)
+
+raster_fname <- "inst/longlake/longlake_depth.tif"
+unlink(raster_fname)
+raster::writeRaster(longlake_depth_raster, raster_fname)
+
---
+++ tests/testthat/test-df_spatial.R
@@ -0,0 +1,51 @@
+context("test-df_spatial.R")
+
+expect_df_spatial <- function(expr) {
+   expr_name <- paste("Expression:", deparse(substitute(expr)))
+   force(expr)
+
+   expect_true(all(c("x", "y", "feature_id") %in% colnames(df_spatial(expr))), info = expr_name)
+   expect_is(df_spatial(expr), "data.frame", info = expr_name)
+   expect_is(df_spatial(expr), "tbl_df", info = expr_name)
+}
+
+test_that("duplicate column name fixing works", {
+  tbl <- tibble::as_tibble(list(x = 1:5, y = 1:5, x = letters[1:5]), validate = FALSE)
+  expect_identical(
+    expect_message(fix_duplicate_cols(tbl), "Renamed columns"),
+    tibble::tibble(x = 1:5, y = 1:5, x..3 = letters[1:5])
+  )
+
+  tbl2 <- tibble::as_tibble(list(x = 1:5, y = 1:5, z = letters[1:5]))
+  expect_identical(
+    expect_silent(fix_duplicate_cols(tbl2)),
+    tbl2
+  )
+})
+
+test_that("Spatial* objects are fortified correctly", {
+  # load the long lake test data
+  load_longlake_data()
+
+  # SpatialPoints
+  spoints_df <- as(longlake_depthdf, "Spatial")
+  spoints <- sp::SpatialPoints(spoints_df, proj4string = spoints_df@proj4string)
+  expect_df_spatial(spoints)
+  expect_equal(nrow(df_spatial(spoints)), length(spoints))
+
+  # SpatialPointsDataFrame
+  expect_df_spatial(spoints_df)
+  expect_equal(nrow(df_spatial(spoints_df)), nrow(spoints_df))
+
+  # SpatialLines
+  splines_df <- as(sf::st_zm(longlake_roadsdf), "Spatial")
+  splines <- sp::SpatialLines(splines_df@lines, proj4string = splines_df@proj4string)
+  line <- splines@lines[[1]]@Lines[[1]]
+  lines <- splines@lines[[1]]
+
+
+  df_spatial(line)
+  df_spatial(lines)
+  df_spatial(splines)
+
+})
