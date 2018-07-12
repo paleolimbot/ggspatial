e9f283408a2fe5d9824e2ace6c083bd4e53ecb7f
Dewey Dunnington <dewey@fishandwhistle.net>
2018-07-12 10:14:12 -0300

    fix CRAN problems of file size and test run time

--- tests/testthat/test-annotation-map-tile.R
+++ tests/testthat/test-annotation-map-tile.R
@@ -1,68 +1,73 @@
 context("test-geom-osm.R")
 
-test_that("geom_osm works as intended", {
-  load_longlake_data()
+# max test length was exceeded on CRAN, so these tests are skipped
+if (identical(Sys.getenv("NOT_CRAN"), "true")) {
 
-  expect_message(
-    print(
-      ggplot() +
-        annotation_map_tile(zoom = 13, cachedir = system.file("rosm.cache", package = "ggspatial")) +
-        geom_sf(data = longlake_waterdf, fill = NA, col = "grey50") +
-        labs("This plot sould have they grey outlines line up with the OSM map beneath")
-    ),
-    "Zoom: 13"
-  )
+  test_that("geom_osm works as intended", {
+    load_longlake_data()
 
-  expect_message(
-    print(
-      ggplot() +
-        annotation_map_tile(zoom = 13, cachedir = system.file("rosm.cache", package = "ggspatial")) +
-        geom_sf(data = longlake_waterdf, fill = NA, col = "grey50") +
-        coord_sf(crs = 26920) +
-        labs("This plot sould have they grey outlines line up with the OSM map beneath")
-    ),
-    "Zoom: 13"
-  )
+    expect_message(
+      print(
+        ggplot() +
+          annotation_map_tile(zoom = 13, cachedir = system.file("rosm.cache", package = "ggspatial")) +
+          geom_sf(data = longlake_waterdf, fill = NA, col = "grey50") +
+          labs("This plot sould have they grey outlines line up with the OSM map beneath")
+      ),
+      "Zoom: 13"
+    )
 
-  expect_message(
-    print(
-      ggplot() +
-        annotation_map_tile(zoom = 13, cachedir = system.file("rosm.cache", package = "ggspatial")) +
-        geom_sf(data = longlake_waterdf, fill = NA, col = "grey50") +
-        coord_sf(crs = 3857) +
-        labs("This plot sould have they grey outlines line up with the OSM map beneath")
-    ),
-    "Zoom: 13"
-  )
+    expect_message(
+      print(
+        ggplot() +
+          annotation_map_tile(zoom = 13, cachedir = system.file("rosm.cache", package = "ggspatial")) +
+          geom_sf(data = longlake_waterdf, fill = NA, col = "grey50") +
+          coord_sf(crs = 26920) +
+          labs("This plot sould have they grey outlines line up with the OSM map beneath")
+      ),
+      "Zoom: 13"
+    )
 
-  expect_message(
-    print(
-      ggplot() +
-        annotation_map_tile(zoom = 13, cachedir = system.file("rosm.cache", package = "ggspatial")) +
-        geom_sf(data = longlake_waterdf, fill = NA, col = "grey50") +
-        coord_sf(crs = 3978) +
-        labs("This plot sould have they grey outlines line up with the OSM map beneath")
-    ),
-    "Zoom: 13"
-  )
+    expect_message(
+      print(
+        ggplot() +
+          annotation_map_tile(zoom = 13, cachedir = system.file("rosm.cache", package = "ggspatial")) +
+          geom_sf(data = longlake_waterdf, fill = NA, col = "grey50") +
+          coord_sf(crs = 3857) +
+          labs("This plot sould have they grey outlines line up with the OSM map beneath")
+      ),
+      "Zoom: 13"
+    )
 
-  expect_message(
-    print(
-      ggplot() +
-        annotation_map_tile(
-          data = tibble::tibble(
-            type = c("osm", "stamenbw", "cartolight", "cartodark"),
-            zoom = 13
-          ),
-          cachedir = system.file("rosm.cache", package = "ggspatial"),
-          mapping = aes(type = type, zoom = zoom)
-        ) +
-        geom_sf(data = longlake_waterdf, fill = NA, col = "grey50") +
-        coord_sf(crs = 3857) +
-        facet_wrap(~type) +
-        labs(caption = "this should have the maptypes correspond to the backdrop")
-    ),
-    "Zoom: 13"
-  )
+    expect_message(
+      print(
+        ggplot() +
+          annotation_map_tile(zoom = 13, cachedir = system.file("rosm.cache", package = "ggspatial")) +
+          geom_sf(data = longlake_waterdf, fill = NA, col = "grey50") +
+          coord_sf(crs = 3978) +
+          labs("This plot sould have they grey outlines line up with the OSM map beneath")
+      ),
+      "Zoom: 13"
+    )
 
-})
+    expect_message(
+      print(
+        ggplot() +
+          annotation_map_tile(
+            data = tibble::tibble(
+              type = c("osm", "stamenbw", "cartolight", "cartodark"),
+              zoom = 13
+            ),
+            cachedir = system.file("rosm.cache", package = "ggspatial"),
+            mapping = aes(type = type, zoom = zoom)
+          ) +
+          geom_sf(data = longlake_waterdf, fill = NA, col = "grey50") +
+          coord_sf(crs = 3857) +
+          facet_wrap(~type) +
+          labs(caption = "this should have the maptypes correspond to the backdrop")
+      ),
+      "Zoom: 13"
+    )
+
+  })
+
+}
