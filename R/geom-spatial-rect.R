abb2e22f2abd30ceff66c8b24f15f3895faf2e56
Dewey Dunnington <dewey@fishandwhistle.net>
2020-01-14 10:20:46 -0400

    add rectangle plotting

---
+++ R/geom-spatial-rect.R
@@ -0,0 +1,166 @@
+
+#' Projected rectangular regions
+#'
+#' If you need to plot a [sf::st_bbox()], use [layer_spatial()] instead.
+#' While the implementation is slightly differrent, these functions are
+#' intended to behave identically to [ggplot2::geom_rect()] and
+#' [ggplot2::geom_tile()].
+#'
+#' @inheritParams stat_spatial_identity
+#' @inheritParams annotation_spatial_hline
+#' @param linejoin How corners should be joined
+#'
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' tile_df <- expand.grid(
+#'   x = seq(-140, -52, by = 20),
+#'   y = seq(40, 70, by = 10)
+#' )
+#'
+#' ggplot(tile_df, aes(x, y)) +
+#'   geom_spatial_tile(crs = 4326) +
+#'   coord_sf(crs = 3979)
+#'
+#' # the same plot using geom_spatial_rect()
+#' ggplot(
+#'   tile_df,
+#'   aes(xmin = x - 10, xmax = x + 10, ymin = y - 5, ymax = y + 5)
+#' ) +
+#'   geom_spatial_rect(crs = 4326) +
+#'   coord_sf(crs = 3979)
+#'
+geom_spatial_rect <- function(mapping = NULL, data = NULL,
+                              ...,
+                              crs = NULL,
+                              linejoin = "mitre",
+                              na.rm = FALSE,
+                              show.legend = NA,
+                              inherit.aes = TRUE) {
+  ggplot2::layer(
+    data = data,
+    mapping = mapping,
+    stat = StatSpatialRect,
+    geom = ggplot2::GeomSf,
+    position = "identity",
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      crs = crs,
+      linejoin = linejoin,
+      legend = "polygon",
+      na.rm = na.rm,
+      ...
+    )
+  )
+}
+
+#' @rdname geom_spatial_rect
+#' @export
+geom_spatial_tile <- function(mapping = NULL, data = NULL,
+                              ...,
+                              crs = NULL,
+                              linejoin = "mitre",
+                              na.rm = FALSE,
+                              show.legend = NA,
+                              inherit.aes = TRUE) {
+  ggplot2::layer(
+    data = data,
+    mapping = mapping,
+    stat = StatSpatialTile,
+    geom = ggplot2::GeomSf,
+    position = "identity",
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      crs = crs,
+      linejoin = linejoin,
+      legend = "polygon",
+      na.rm = na.rm,
+      ...
+    )
+  )
+}
+
+#' @rdname geom_spatial_rect
+#' @export
+StatSpatialRect <- ggplot2::ggproto(
+  "StatSpatialRect", ggplot2::Stat,
+
+  required_aes = c("xmin", "xmax", "ymin", "ymax"),
+
+  compute_layer = function(self, data, params, layout) {
+    # add dest CRS to parameters
+    params$crs_dest <- sf::st_crs(layout$coord_params$crs)
+    ggplot2::ggproto_parent(ggplot2::Stat, self)$compute_layer(data, params, layout)
+  },
+
+  compute_panel = function(self, data, scales, crs, crs_dest) {
+    # source CRS
+    if(is.null(crs)) {
+      message("Assuming `crs = 4326` in stat_spatial_rect()")
+      crs <- sf::st_crs(4326)
+    } else {
+      crs <- sf::st_crs(crs)
+    }
+
+    bboxes <- lapply(seq_len(nrow(data)), function(i) {
+      sf::st_bbox(
+        c(
+          xmin = data$xmin[i],
+          ymin = data$ymin[i],
+          xmax = data$xmax[i],
+          ymax = data$ymax[i]
+        ),
+        crs = crs
+      )
+    })
+
+    geometry <- lapply(bboxes, function(x) sf_bbox_to_sf(x, detail = 10)$geometry[[1]])
+    data$geometry <- do.call(sf::st_sfc, c(geometry, list(crs = crs)))
+    data$geometry <- sf::st_transform(data$geometry, crs = crs_dest)
+
+    # update xmin/xmax/ymin/ymax for proper scale training
+    projected_bbox <- sf::st_bbox(data$geometry)
+    data$xmin <- projected_bbox["xmin"]
+    data$ymin <- projected_bbox["ymin"]
+    data$xmax <- projected_bbox["xmax"]
+    data$ymax <- projected_bbox["ymax"]
+
+    data
+  }
+)
+
+#' @rdname geom_spatial_rect
+#' @export
+StatSpatialTile <- ggplot2::ggproto(
+  "StatSpatialTile", StatSpatialRect,
+
+  setup_data = function(data, params) {
+    if (all(is.na(data$width))) {
+      data$width <- ggplot2::resolution(data$x, FALSE)
+    }
+
+    if (all(is.na(data$height))) {
+      data$height <- ggplot2::resolution(data$y, FALSE)
+    }
+
+    x <- NULL; rm(x)
+    height <- NULL; rm(height)
+    width <- NULL; rm(width)
+
+    transform(
+      data,
+      xmin = x - width / 2,
+      xmax = x + width / 2,
+      width = NULL,
+      ymin = y - height / 2,
+      ymax = y + height / 2,
+      height = NULL
+    )
+  },
+
+  required_aes = c("x", "y"),
+  default_aes = aes(height = NA_real_, width = NA_real_)
+)
---
+++ tests/testthat/test-geom-spatial-rect.R
@@ -0,0 +1,54 @@
+
+context("test-geom-spatial-rect")
+
+test_that("geom_spatial_rect() works", {
+  # Canada!
+  box <- sf::st_bbox(c(xmin = -140, xmax = -52, ymin = 41, ymax = 83), crs = 4326)
+  tile_df <- expand.grid(
+    xmin = seq(-140, -52, by = 20),
+    ymin = seq(40, 70, by = 10)
+  )
+
+  tile_df$xmax <- tile_df$xmin + 20
+  tile_df$ymax <- tile_df$ymin + 10
+  tile_df$x <- (tile_df$xmin + tile_df$xmax) / 2
+  tile_df$y <- (tile_df$ymin + tile_df$ymax) / 2
+  tile_df$width <- 20
+  tile_df$height <- 10
+
+
+  p <- ggplot(
+    tile_df,
+    aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, x = x, y = y)
+  ) +
+    coord_sf(crs = 3979)
+
+  expect_message(
+    ggplot2::ggplot_build(p + geom_spatial_rect()),
+    "Assuming `crs = 4326`"
+  )
+
+  expect_silent(
+    ggplot2::ggplot_build(p + geom_spatial_rect(crs = 4326))
+  )
+
+  vdiffr::expect_doppelganger(
+    "geom_spatial_rect()",
+    p + geom_spatial_rect(crs = 4326)
+  )
+
+  vdiffr::expect_doppelganger(
+    "geom_spatial_rect(), mapped aes",
+    p + geom_spatial_rect(aes(fill = factor(xmin)), crs = 4326)
+  )
+
+  vdiffr::expect_doppelganger(
+    "geom_spatial_tile(), mapped dims",
+    p + geom_spatial_tile(aes(height = 7.5, width = 5), crs = 4326)
+  )
+
+  vdiffr::expect_doppelganger(
+    "geom_spatial_tile(), auto dims",
+    p + geom_spatial_tile(crs = 4326)
+  )
+})
