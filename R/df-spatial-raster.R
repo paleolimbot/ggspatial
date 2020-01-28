397796456dfc4b1833c88c7f944854844dd6432c
Dewey Dunnington <dewey@fishandwhistle.net>
2020-01-27 22:38:20 -0800

    fix default pixel positioning (closes #41)

--- R/df-spatial-raster.R
+++ R/df-spatial-raster.R
@@ -1,6 +1,6 @@
 
 #' @export
-df_spatial.Raster <- function(x, ...) {
+df_spatial.Raster <- function(x, ..., hjust = 0.5, vjust = 0.5, na.rm = FALSE) {
   # get values in a data frame
   nrows <- x@nrows
   ncols <- x@ncols
@@ -15,12 +15,19 @@ df_spatial.Raster <- function(x, ...) {
   nbands <- ncol(fused) - 2
   names(fused) <- c("x", "y", paste0("band", seq_len(nbands)))
 
+  if (na.rm) {
+    for (i in seq_len(nbands)) {
+      fused <- fused[!is.na(fused[[paste0("band", i)]]), ]
+    }
+  }
+
   # fix x and y to be physical coordinates using the bbox
   bbox_width <- bbox[1, 2] - bbox[1, 1]
   bbox_height <- bbox[2, 1] - bbox[2, 2]
+  res <- raster::res(x)
 
-  fused$x <- bbox[1, 1] + (fused$x - 1) / ncols * bbox_width
-  fused$y <- bbox[2, 1] + (fused$y - nrows) / nrows * bbox_height
+  fused$x <- bbox[1, 1] + res[1] * hjust + (fused$x - 1) / ncols * bbox_width
+  fused$y <- bbox[2, 1] + res[2] * vjust + (fused$y - nrows) / nrows * bbox_height
 
   tibble::as_tibble(fused)
 }
--- tests/testthat/test-df-spatial-raster.R
+++ tests/testthat/test-df-spatial-raster.R
@@ -26,3 +26,36 @@ test_that("Raster* objects are converted properly by df_spatial", {
     ggplot(df_spatial(longlake_osm)) + ggplot2::geom_raster(aes(x, y, fill = band1))
   )
 })
+
+test_that("x and y coordinates are exactly right for raster df", {
+  rst <- raster::raster(
+    matrix(
+      rep_len(c(0, 1), 9),
+      ncol = 3
+    ),
+    xmn = 0, xmx = 3,
+    ymn = 0, ymx = 6
+  )
+
+  df <- df_spatial(rst)
+
+  expect_setequal(df$x, c(0.5, 1.5, 2.5))
+  expect_setequal(df$y, c(1, 3, 5))
+
+  df2 <- df_spatial(rst, hjust = 0, vjust = 0)
+  expect_setequal(df2$x, c(0, 1, 2))
+  expect_setequal(df2$y, c(0, 2, 4))
+
+  df3 <- df_spatial(rst, hjust = 1, vjust = 1)
+  expect_setequal(df3$x, c(1, 2, 3))
+  expect_setequal(df3$y, c(2, 4, 6))
+})
+
+test_that("na.rm works on df_spatial.Raster()", {
+  load_longlake_data(which = "longlake_osm")
+  df <- df_spatial(longlake_osm)
+  expect_true(any(is.na(df$band1)))
+
+  df_finite <- df_spatial(longlake_osm, na.rm = TRUE)
+  expect_false(any(is.na(df_finite$band1)))
+})
