5fb07e6683aebccdba2bc5ab2bcf611e49ca896a
Dewey Dunnington <dewey@fishandwhistle.net>
2022-11-20 23:27:02 -0400

    fix more deprecations from ggplot2

--- R/ggspatial-package.R
+++ R/ggspatial-package.R
@@ -4,6 +4,7 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom ggplot2 after_stat
 ## usethis namespace: end
 NULL
 
